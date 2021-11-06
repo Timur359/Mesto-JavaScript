7fc9163c657771c8b869143a0cc2224fc0084201
Timur359 <88931387+Timur359@users.noreply.github.com>
2021-11-06 18:57:28 +0300

    Delete scripts directory

--- scripts/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inputErrorClass: 'popup__input_type_error',
-    inactiveButtonClass: 'popup__button_disabled',
-};
