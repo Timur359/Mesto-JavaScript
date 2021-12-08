4072d7a88ebbe161d1690fd7b7eb2f23b968efcc
Timur359 <88931387+Timur359@users.noreply.github.com>
2021-12-08 19:07:45 +0300

    Add files via upload

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+const editButton = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup_edit');
+const popupZoom = document.querySelector('.popup_zoom');
+const profileNameInput = document.querySelector('.popup__input_data_name');
+const profileAboutInput = document.querySelector('.popup__input_data_about');
+
+const validationObject = {
+ formSelector: '.popup__form',
+ inputSelector: '.popup__input',
+ submitButtonSelector: '.popup__button',
+ inputErrorClass: 'popup__input_type_error',
+ inactiveButtonClass: 'popup__button_disabled',
+ errorClass: 'popup__error_visible',
+};
+
+const selectorObject = {
+ popupProfileSelector: '.popup_edit',
+ popupAddCardSelector: '.popup_add',
+ profileNameSelector: '.profile__name',
+ profileAboutSelector: '.profile__about',
+ inputNameSelector: '.popup__input_data_name',
+ inputAboutSelector: '.popup__input_data_about',
+};
+
+const listElement = document.querySelector('.elements');
+const templateItem = document.querySelector('#template').content;
+const popupAdd = document.querySelector('.popup_add');
+const buttonAdd = document.querySelector('.profile__add-button');
+
+export {
+ editButton,
+ popupEdit,
+ popupZoom,
+ validationObject,
+ selectorObject,
+ listElement,
+ templateItem,
+ popupAdd,
+ buttonAdd,
+ profileNameInput,
+ profileAboutInput,
+};
