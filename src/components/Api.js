8d935bd212099dbebb42f89f3ba643e05c552978
Timur359 <88931387+Timur359@users.noreply.github.com>
2021-12-15 11:55:37 +0300

    Add files via upload

---
+++ src/components/Api.js
@@ -0,0 +1,97 @@
+export default class Api {
+ constructor({ baseUrl, headers }) {
+  this._baseUrl = baseUrl;
+  this._userUrl = `${this._baseUrl}/users/me`;
+  this._cardsUrl = `${this._baseUrl}/cards`;
+  this._likesUrl = `${this._baseUrl}/cards/likes`;
+  //this._token = headers['authorization'];
+  this._headers = headers;
+ }
+
+ //Получение информации о пользователе
+
+ getUserData() {
+  return fetch(this._userUrl, {
+   headers: this._headers,
+  }).then(this._checkResponse);
+ }
+
+ //Сохранение редактирования профиля
+
+ saveUserChanges({ name, about }) {
+  return fetch(this._userUrl, {
+   method: 'PATCH',
+   headers: this._headers,
+   body: JSON.stringify({
+    name: name,
+    about: about,
+   }),
+  }).then(this._checkResponse);
+ }
+
+ //Получение массива карточек с сервера
+
+ getInitialCards() {
+  return fetch(this._cardsUrl, {
+   headers: this._headers,
+  }).then(this._checkResponse);
+ }
+
+ //Добавление карточек на сервер
+
+ postNewCard({ name, link }) {
+  return fetch(this._cardsUrl, {
+   method: 'POST',
+   headers: this._headers,
+   body: JSON.stringify({
+    name: name,
+    link: link,
+   }),
+  }).then(this._checkResponse);
+ }
+
+ //Удаление карточек
+
+ deleteCard(cardId) {
+  return fetch(`${this._cardsUrl}/${cardId}`, {
+   method: 'DELETE',
+   headers: this._headers,
+  }).then(this._checkResponse);
+ }
+
+ //"Лайк" карточек
+
+ likedCard(cardId) {
+  return fetch(`${this._likesUrl}/${cardId}`, {
+   method: 'PUT',
+   headers: this._headers,
+  }).then(this._checkResponse);
+ }
+
+ //Снятие "лайка"
+ dislikedCard(cardId) {
+  return fetch(`${this._likesUrl}/${cardId}`, {
+   method: 'DELETE',
+   headers: this._headers,
+  }).then(this._checkResponse);
+ }
+
+ //Обновление аватара
+
+ changedAvatar(src) {
+  return fetch(`${this._userUrl}/avatar`, {
+   method: 'PATCH',
+   headers: this._headers,
+   body: JSON.stringify({
+    avatar: src.link,
+   }),
+  }).then(this._checkResponse);
+ }
+
+ _checkResponse(res) {
+  if (!res.ok) {
+   return Promise.reject(`Error: ${res.status}`);
+  }
+  return res.json();
+ }
+}
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,44 @@
+import Popup from './Popup.js';
+
+export default class PopupWithForm extends Popup {
+ constructor(popupSelector, handleSubmit) {
+  super(popupSelector);
+  this._handleSubmit = handleSubmit;
+  this._popupForm = this._popup.querySelector('.popup__form');
+  this._inputList = this._popup.querySelectorAll('.popup__input');
+  this._button = this._popupForm.querySelector('.popup__button');
+  this._textButton = this._button.textContent;
+ }
+
+ close() {
+  super.close();
+  this._popupForm.reset();
+ }
+
+ // метод, который собирает данные всех полей формы
+ _getInputValues() {
+  this._formValues = {};
+
+  this._inputList.forEach((input) => {
+   this._formValues[input.name] = input.value;
+  });
+
+  return this._formValues;
+ }
+
+ setEventListeners() {
+  super.setEventListeners();
+  this._popupForm.addEventListener('submit', (evt) => {
+   evt.preventDefault();
+   this._handleSubmit(this._getInputValues());
+  });
+ }
+
+ renderSaving(status) {
+  if (status) {
+   this._button.textContent = `Cохранение...`;
+  } else {
+   this._button.textContent = this._textButton;
+  }
+ }
+}
